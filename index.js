bd20de84a46cacee3af5ed2abc4f3e9988bd8cc1
Jenn Robbins <Jennrobbins3@gmail.com>
2022-02-21 20:39:25 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+  
+function superbowlWin(array){
+    const denverWins = array.find(array => array.result === "W")
+return denverWins ? denverWins.year : undefined
+}
+
+       
